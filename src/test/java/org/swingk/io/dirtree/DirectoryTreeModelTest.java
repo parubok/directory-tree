f168774221c8a96e68362406cf433f0e63bf4e44
parubok <parubok4@gmail.com>
2021-03-14 21:36:19 +0200

    Updates.

--- src/test/java/org/swingk/io/dirtree/DirectoryTreeModelTest.java
+++ src/test/java/org/swingk/io/dirtree/DirectoryTreeModelTest.java
@@ -3,6 +3,8 @@ package org.swingk.io.dirtree;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
 public class DirectoryTreeModelTest {
     @Test
     public void basicTest() {
@@ -10,7 +12,12 @@ public class DirectoryTreeModelTest {
         Assertions.assertNotNull(model.getRoot());
         Assertions.assertEquals(1, model.getChildCount(model.getRoot()));
         Assertions.assertNotNull(model.getFileSystemNode());
-        Assertions.assertFalse(model.getFileSystemRootNodes().isEmpty());
-        Assertions.assertEquals(model.getFileSystemRootNodes().size(), model.getChildCount(model.getFileSystemNode()));
+
+        List<DirectoryNode> fileSystemRootNodes = model.getFileSystemRootNodes();
+        Assertions.assertFalse(fileSystemRootNodes.isEmpty());
+        Assertions.assertEquals(fileSystemRootNodes.size(), model.getChildCount(model.getFileSystemNode()));
+        for (DirectoryNode fileSystemRootNode : fileSystemRootNodes) {
+            Assertions.assertEquals(0, fileSystemRootNode.getDirectory().getNameCount());
+        }
     }
 }
